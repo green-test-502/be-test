20e8b77a42d95cc801039a32af9e864339366a03
sbsteacher <id@gmail.com>
2026-03-06 10:50:09 +0900

    first

---
+++ src/main/java/com/green/greentest/GreenTestApplication.java
@@ -0,0 +1,13 @@
+package com.green.greentest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GreenTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/green/greentest/GreenTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.green.greentest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
